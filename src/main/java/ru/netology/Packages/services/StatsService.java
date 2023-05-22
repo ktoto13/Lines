688a486cb5b8dc85b198891c03e602a50b290fff
Natis <traffkina@mail,ru>
2023-05-22 21:08:23 +0300

    Readme

---
+++ src/main/java/ru/netology/Packages/services/StatsService.java
@@ -0,0 +1,64 @@
+package ru.netology.Packages.services;
+
+public class StatsService {
+    public int SumAllSales(int[] sales) {
+        int sum = 0;
+        for (int sale : sales) {
+            sum = sum + sale;
+        }
+        return sum;
+    }
+
+    public int AvgSumSales(int[] sales) {
+        return SumAllSales(sales) / sales.length;
+    }
+
+    public int sumMaxMonth(long[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+    }
+
+
+    public int sumMinMonth(long[] sales) {
+        int minMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+        return minMonth + 1;
+    }
+
+    public int amountMonthUnderAvg(int[] sales) {
+        int count = 0;
+        int salesAvg = AvgSumSales(sales);
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] < salesAvg) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int amountMonthOverAvg(int[] sales) {
+        int count = 0;
+        int salesAvg = AvgSumSales(sales);
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] > salesAvg) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
---
+++ src/test/java/ru/netology/Packages/services/StatsServiceTest.java
@@ -0,0 +1,83 @@
+package ru.netology.Packages.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+
+    @Test
+    void shouldSumAllSales() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 180;
+
+        int actual = service.SumAllSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+    void shouldAvgSumSales() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 15;
+
+        int actual = service.AvgSumSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldSumMaxMonth() {
+        StatsService service = new StatsService();
+        long[] maxMonth = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 8;
+
+        long actual = service.sumMaxMonth(maxMonth);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldSumMinMonth() {
+        StatsService service = new StatsService();
+        long[] minMonth = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 9;
+
+        long actual = service.sumMinMonth(minMonth);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldAmountMonthUnderAvg() {
+        StatsService service = new StatsService();
+        int[] amountMonthUnderAvg = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        long actual = service.amountMonthUnderAvg(amountMonthUnderAvg);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldamountMonthOverAvg() {
+        StatsService service = new StatsService();
+        int[] amountMonthOverAvg = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        long actual = service.amountMonthOverAvg(amountMonthOverAvg);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+
+}
+
+
